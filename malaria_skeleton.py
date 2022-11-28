e6c4ac950c7029332c600bfcbc21f63ffc27202f
SvandeVosse <simo888@live.nl>
2022-11-28 22:27:05 +0100

    Simulation first working(?) version

--- malaria_skeleton.py
+++ malaria_skeleton.py
@@ -4,10 +4,22 @@ import malaria_visualize
 
 
 class Model:
-    def __init__(self, width=50, height=50, nHuman=10, nMosquito=20,
-                 initMosquitoHungry=0.5, initHumanInfected=0.2,
-                 humanInfectionProb=0.25, mosquitoInfectionProb=0.9,
-                 biteProb=1.0):
+    def __init__(
+        self,
+        width=50,
+        height=50,
+        nHuman=10,
+        nMosquito=20,
+        initMosquitoHungry=0.5,
+        initHumanInfected=0.2,
+        humanInfectionProb=0.25,
+        mosquitoInfectionProb=0.9,
+        biteProb=1.0,
+        mealInterval=5,
+        infectionPeriod=3,
+        immuntiyPeriod=10,
+        humanDeathByInfectionProb=0.3,
+    ):
         """
         Model parameters
         Initialize the model with the width and height parameters.
@@ -19,6 +31,10 @@ class Model:
         self.humanInfectionProb = humanInfectionProb
         self.mosquitoInfectionProb = mosquitoInfectionProb
         self.biteProb = biteProb
+        self.mealInterval = mealInterval
+        self.infectionPeriod = infectionPeriod
+        self.immunityPeriod = immuntiyPeriod
+        self.humanDeathByInfectionProb = humanDeathByInfectionProb
         # etc.
 
         """
@@ -44,17 +60,35 @@ class Model:
         objects is initialized with the "infected" state.
         """
         humanPopulation = []
+        humanPositions = []
         for i in range(self.nHuman):
             x = np.random.randint(self.width)
             y = np.random.randint(self.height)
+
             """
-            To implement: Humans may not have overlapping positions.
+            Humans may not have overlapping positions.
             """
+
+            # find new position if position is already taken
+            while (x, y) in humanPositions:
+                # print(f"{(x, y)} already in positions {humanPositions}")
+
+                # generate new coordinates
+                x = np.random.randint(self.width)
+                y = np.random.randint(self.height)
+
+            # store position
+            humanPositions.append((x, y))
+
+            # determine if human is infected or not
             if (i / self.nHuman) <= initHumanInfected:
-                state = 'I'  # I for infected
+                state = "I"  # I for infected
             else:
-                state = 'S'  # S for susceptible
+                state = "S"  # S for susceptible
+
+            # add human instance to list of humans
             humanPopulation.append(Human(x, y, state))
+
         return humanPopulation
 
     def set_mosquito_population(self, initMosquitoHungry):
@@ -86,20 +120,74 @@ class Model:
         """
         for i, m in enumerate(self.mosquitoPopulation):
             m.move(self.height, self.width)
+
+            # possibly bite human
             for h in self.humanPopulation:
-                if m.position == h.position and m.hungry\
-                   and np.random.uniform() <= self.biteProb:
-                    m.bite(h, self.humanInfectionProb,
-                           self.mosquitoInfectionProb)
-        """
-        To implement: set the hungry state from false to true after a
-                     number of time steps has passed.
-        """
+                if (
+                    m.position == h.position
+                    and m.hungry
+                    and np.random.uniform() <= self.biteProb
+                ):
+                    m.bite(h, self.humanInfectionProb, self.mosquitoInfectionProb)
+
+            """
+            Set the hungry state from false to true after a
+                        number of time steps has passed.
+            """
+            if not m.hungry:
+                m.lastMeal += 1
+                if (
+                    m.lastMeal > self.mealInterval
+                ):  # they are very punctual, optional: decay rate probability
+                    m.hungry = True
+                    m.lastMeal = 0
+
+            # print hunger state of every Mosquito
+            # if m.hungry:
+            #     hunger_str = "hungry"
+            # else:
+            #     hunger_str = "satisfied"
+            # print(f"Mosquito {i} is {hunger_str}.")
 
         for j, h in enumerate(self.humanPopulation):
             """
             To implement: update the human population.
             """
+
+            if h.state == "I":
+                # add infection to the total when human just got infected
+                if h.lastInfection == 0:
+                    self.infectedCount += 1
+                    print("Infected!")
+
+                if h.lastInfection > self.infectionPeriod:
+                    if np.random.uniform() <= self.humanDeathByInfectionProb:
+                        """
+                        Human dies
+                        """
+                        self.deathCount += 1
+                        print("Dead!")
+
+                        # human reincarnates on same position
+                        self.humanPopulation[j] = Human(*h.position, state="S")
+                    else:
+                        """
+                        Human is immune
+                        """
+                        h.state = "Immune"
+                        print("Immunity!")
+                        h.lastImmunity = 0
+                else:
+                    # add time to last infection
+                    h.lastInfection += 1
+
+            elif h.state == "Immune":
+                h.lastImmunity += 1
+                if h.lastImmunity > self.immunityPeriod:
+                    h.state = "S"
+                    print("Susceptible!")
+
+            ## to implement: human dies of natural causes
 
         """
         To implement: update the data/statistics e.g. infectedCount,
@@ -118,6 +206,7 @@ class Mosquito:
         self.position = [x, y]
         self.hungry = hungry
         self.infected = False
+        self.lastMeal = 0  # time since last meal
 
     def bite(self, human, humanInfectionProb, mosquitoInfectionProb):
         """
@@ -127,10 +216,11 @@ class Mosquito:
         mosquito can be infected.
         After a mosquito bites it is no longer hungry.
         """
-        if self.infected and human.state == 'S':
+        if self.infected and human.state == "S":
             if np.random.uniform() <= humanInfectionProb:
-                human.state = 'I'
-        elif not self.infected and human.state == 'I':
+                human.state = "I"
+                human.lastInfection = 0
+        elif not self.infected and human.state == "I":
             if np.random.uniform() <= mosquitoInfectionProb:
                 self.infected = True
         self.hungry = False
@@ -162,42 +252,53 @@ class Human:
         """
         self.position = [x, y]
         self.state = state
+        self.lastInfection = 0  # time since last infection
+        self.lastImmunity = 0  # time since last immunity
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """
     Simulation parameters
     """
-    fileName = 'simulation'
+    fileName = "simulation"
     timeSteps = 100
     t = 0
+
+    # whether or not to run simulations and/or plot
+    runSim = True
     plotData = False
-    """
-    Run a simulation for an indicated number of timesteps.
-    """
-    file = open(fileName + '.csv', 'w')
-    sim = Model()
-    vis = malaria_visualize.Visualization(sim.height, sim.width)
-    print('Starting simulation')
-    while t < timeSteps:
-        [d1, d2] = sim.update()  # Catch the data
-        line = str(t) + ',' + str(d1) + ',' + str(d2) + '\n'  # Separate the data with commas
-        file.write(line)  # Write the data to a .csv file
-        vis.update(t, sim.mosquitoPopulation, sim.humanPopulation)
-        t += 1
-    file.close()
-    vis.persist()
+
+    if runSim:
+        """
+        Run a simulation for an indicated number of timesteps.
+        """
+        file = open(fileName + ".csv", "w")
+        sim = Model(width=50, height=50, nHuman=1, nMosquito=20, initHumanInfected=0)
+        vis = malaria_visualize.Visualization(sim.height, sim.width)
+
+        print("Starting simulation")
+        while t < timeSteps:
+            # print(t)
+            [d1, d2] = sim.update()  # Catch the data
+            line = (
+                str(t) + "," + str(d1) + "," + str(d2) + "\n"
+            )  # Separate the data with commas
+            file.write(line)  # Write the data to a .csv file
+            vis.update(t, sim.mosquitoPopulation, sim.humanPopulation)
+            t += 1
+        file.close()
+        vis.persist()
 
     if plotData:
         """
         Make a plot by from the stored simulation data.
         """
-        data = np.loadtxt(fileName+'.csv', delimiter=',')
+        data = np.loadtxt(fileName + ".csv", delimiter=",")
         time = data[:, 0]
         infectedCount = data[:, 1]
         deathCount = data[:, 2]
         plt.figure()
-        plt.plot(time, infectedCount, label='infected')
-        plt.plot(time, deathCount, label='deaths')
+        plt.plot(time, infectedCount, label="infected")
+        plt.plot(time, deathCount, label="deaths")
         plt.legend()
         plt.show()
